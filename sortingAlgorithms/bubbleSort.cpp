43a977ab25504c4b4fef99d3bf8c5f4e7186cd53
“Danell <danelljhamilton@gmail.com>
2025-09-08 02:13:29 -0600

    Timing/Helper/Functions

---
+++ sortingAlgorithms/bubbleSort.cpp
@@ -0,0 +1,2 @@
+#include <vector>
+void bubbleSort(std::vector<int>&) { /* todo: implement; left empty so it prints NOT sorted */ }
---
+++ sortingAlgorithms/sorts.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+void bubbleSort(std::vector<int>& a);
+void selectionSort(std::vector<int>& a);
+void insertionSort(std::vector<int>& a);
+void mergeSort(std::vector<int>& a);  
+void quickSort(std::vector<int>& a);   
