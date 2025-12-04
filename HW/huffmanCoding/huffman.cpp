a7e2fec35aba2144de120c974d25784cda0b5f6c
“Danell <danelljhamilton@gmail.com>
2025-12-03 17:30:30 -0700

    HW4Pt4

--- HW/huffmanCoding/huffman.cpp
+++ HW/huffmanCoding/huffman.cpp
@@ -4,3 +4,33 @@
 #include <iostream>
 #include <iomanip>
 #include <map>
+
+Huffman::Huffman() : root(nullptr) {}
+
+Huffman::~Huffman(){
+    freeTree(root);
+    root = nullptr;
+}
+
+void Huffman::freeTree(Node* node) {
+}
+
+void Huffman::build(const std::string& text) {
+}
+
+void Huffman::buildCodes(Node* node, const std::string& prefix) {
+}
+
+std::string Huffman::encode(const std::string& text) const {
+}
+
+std::string Huffman::decode(const std::string& bits) const {
+}
+
+void Huffman::printCodes() const {
+}
+
+void Huffman::printStats(const std::string& text,
+                         const std::string& bits) const {
+                         }
+                        
