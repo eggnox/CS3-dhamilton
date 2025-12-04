5730c0af8f53112b0b02e3be9c9083a6d1579d31
“Danell <danelljhamilton@gmail.com>
2025-12-03 17:08:02 -0700

    HW4Pt2

--- HW/huffmanCoding/huffman.h
+++ HW/huffmanCoding/huffman.h
@@ -4,6 +4,21 @@
 
 class Huffman {
 public:
+    Huffman();
+    ~Huffman();
+
+    //Build trees
+    void build(const std::string& text);
+
+    //encode and decode
+    std::string encode(const std::string & text) const;
+    std::string decode(const std::string & bits) const;
+
+    //print code + stats
+    void printCodes() const;
+    void printStats(const std::string& text, 
+                    const std:: string& bits) const;
+
 
 private:
 };
