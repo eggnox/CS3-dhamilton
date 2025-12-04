b76a283e9e488b6581c8e70073326651fdace813
“Danell <danelljhamilton@gmail.com>
2025-12-03 17:19:27 -0700

    HW4Pt3

--- HW/huffmanCoding/huffman.h
+++ HW/huffmanCoding/huffman.h
@@ -21,4 +21,21 @@ public:
 
 
 private:
+    struct Node {
+        char ch;
+        int freq;
+        Node* left;
+        Node* right;
+        Node(char c, int f, Node* l = nullptr, Node* r = nullptr)
+            : ch(c), freq(f), left(l), right(r) {}
+    };
+
+    Node* root;                             //root
+    std::map<char, std::string> codes;      //char 
+
+    void freeTree(Node* node);
+    void buildCodes(Node* node, const std::string& prefix);
+
+    Huffman(const Huffman&) = delete;
+    Huffman& operator=(const Huffman&) = delete;
 };
